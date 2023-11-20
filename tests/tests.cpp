dcdfee969a8aa24a5be4b1750448eb38577caa74
hyprd <luuzeish@gmail.com>
2023-11-20 23:46:48 +1300

    Tidy up LDA_IMM

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -5,6 +5,7 @@ class M6502TestSuite : public testing::Test {
 public:
 	NMOS6502 M6502;
 	u32 InstructionCycles = 0;
+	u32 CyclesRan = 0;
 
 	virtual void SetUp() {
 		M6502.Reset();
@@ -19,7 +20,7 @@ TEST_F(M6502TestSuite, LDA_IMM) {
 	InstructionCycles = 2;
 	M6502.Memory[0xFFFC] = 0xA9;
 	M6502.Memory[0xFFFD] = 0x42;
-	M6502.Execute(InstructionCycles);
+	CyclesRan = M6502.Execute(InstructionCycles);
 	ASSERT_EQ(M6502.A, 0x42);
 }
 
@@ -29,7 +30,7 @@ TEST_F(M6502TestSuite, LDA_ABS) {
 	M6502.Memory[0xFFFD] = 0xBC;
 	M6502.Memory[0xFFFE] = 0xA1;
 	M6502.Memory[0xBCA1] = 0x90;
-	u8 CyclesRan = M6502.Execute(InstructionCycles);
+	CyclesRan = M6502.Execute(InstructionCycles);
 	ASSERT_EQ(M6502.A, 0x90);
 	ASSERT_EQ(CyclesRan, InstructionCycles);
 }
